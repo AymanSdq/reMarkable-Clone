382e683352c10f30a90b72390f37dc225d8e5580
AymanSdq <sedqiayman02@gmail.com>
2024-11-06 17:46:54 +0100

    glance section started

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Header from './components/Header'
 import HeroSection from './components/HeroSection'
+import InformationRem from './components/InformationRem'
 import SmallSection from './components/SmallSection'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
           <Header />
           <HeroSection />
           <SmallSection />
+          <InformationRem />
       </main>
   )
 }
