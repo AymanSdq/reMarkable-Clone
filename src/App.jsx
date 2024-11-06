590a26afa33455fce74cd85cbb8abde2d13aefd7
AymanSdq <sedqiayman02@gmail.com>
2024-11-06 14:42:28 +0100

    Header and Hero done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
+import Header from './components/Header'
+import HeroSection from './components/HeroSection'
 
 function App() {
   
 
   return (
       <main>
-              
+          <Header />
+          <HeroSection />
       </main>
   )
 }
