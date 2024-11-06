1d5d26c4c0e003700420c6dde3890ea5057f68cd
AymanSdq <sedqiayman02@gmail.com>
2024-11-06 23:46:37 +0100

    Note Taking and Connect section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import ConnectSection from './components/ConnectSection'
 import Digitalbook from './components/Digitalbook'
 import EyeFriendly from './components/EyeFriendly'
 import Header from './components/Header'
 import HeroSection from './components/HeroSection'
 import InformationRem from './components/InformationRem'
+import NoteTaking from './components/NoteTaking'
 import SmallSection from './components/SmallSection'
 
 function App() {
@@ -17,6 +19,10 @@ function App() {
           <InformationRem />
           <Digitalbook />
           <EyeFriendly />
+          {/* // TODO : add the section that left : Share your ideas. Live. */}
+          <NoteTaking />
+          <ConnectSection />
+
       </main>
   )
 }
